046fa1c54f44ce8e00fb077beca06a20d9247bfe
Jiayi An <an.jiayi@epa.gov>
2022-11-08 08:20:05 -0500

    move db function into database.go

--- database.go
+++ database.go
@@ -1,9 +1,31 @@
 package main
 
-import "database/sql"
-
-var (
-	// DBCon is the connection handle
-	// for the database
-	DBCon *sql.DB
+import (
+	"database/sql"
+	"fmt"
+	"log"
 )
+
+func getDBList(db *sql.DB) []string {
+	// Query the DB
+	var row string
+	rows, err := db.Query(`SHOW DATABASES;`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var listSlice []string
+	for rows.Next() {
+		err := rows.Scan(&row)
+		if err != nil {
+			log.Fatal(err)
+		}
+		listSlice = append(listSlice, row)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DB list is here :", listSlice)
+	return listSlice
+}
--- main.go
+++ main.go
@@ -8,35 +8,10 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 )
 
 type DBList struct {
 	DbName string
-}
-
-func getDBList(db *sql.DB) []string {
-	// Query the DB
-	var row string
-	rows, err := db.Query(`SHOW DATABASES;`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	var listSlice []string
-	for rows.Next() {
-		err := rows.Scan(&row)
-		if err != nil {
-			log.Fatal(err)
-		}
-		listSlice = append(listSlice, row)
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("DB list is here :", listSlice)
-	return listSlice
 }
 
 func main() {
